7e322af9c55940f84f469b483e085e66d6f5fff2
Aleph-Zer0 <62743855+SlavaSTiwari@users.noreply.github.com>
2022-02-07 18:22:35 +0300

    Lab-3
    TicTacToe game v/s AI using the Minimax algorithm along with Alpha-beta pruning. (Spoiler alert: It's unbeatable)

---
+++ Lab-3/Main_Game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+
+#include "Board.h"
+#include "Logic.h"
+#include "TicTacToe.h"
+
+int main()
+{
+	Game_start();
+
+	return 0;
+}
